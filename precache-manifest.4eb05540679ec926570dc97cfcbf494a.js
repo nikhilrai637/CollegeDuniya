438d2349931ad94ccf580b4e3a1595d4f7eccc8e
Nikhil Rai <nikhilrai637@gmail.com>
2020-10-31 15:37:26 +0530

    Updates

---
+++ precache-manifest.4eb05540679ec926570dc97cfcbf494a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c431312ebab5c1ea3ef04f1c18827a4d",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "252eced4bdb89a79c53d",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/2.64715bce.chunk.js"
+  },
+  {
+    "revision": "a38d846d3316db5ec944",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/main.320eae28.chunk.js"
+  },
+  {
+    "revision": "2586becbdf08f82fe84e",
+    "url": "%7Busername%7D.github.io/%7Brepo-name%7D/static/js/runtime-main.19122ad9.js"
+  }
+]);
